21b44eac10dda69d6f9a7a44b7a493b461ab21c7
Marten Seemann <martenseemann@gmail.com>
2019-09-22 12:42:35 +0700

    use the REQUESTS environment variable
    and increase the wait-for-it timeout

--- run_endpoint.sh
+++ run_endpoint.sh
@@ -6,11 +6,11 @@ set -e
 
 if [ "$ROLE" == "client" ]; then
     # Wait for the simulator to start up.
-    /wait-for-it.sh sim:57832 -s -t 5
+    /wait-for-it.sh sim:57832 -s -t 10
     echo "Starting QUIC client..."
     echo "Client params: $CLIENT_PARAMS"
     echo "Test case: $TESTCASE"
-    go run client/main.go $CLIENT_PARAMS
+    go run client/main.go $CLIENT_PARAMS $REQUESTS
 else
     echo "Running QUIC server."
     go run server/main.go "$@"
